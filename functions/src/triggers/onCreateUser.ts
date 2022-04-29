0d4437b5f8841224bd1413df1b430e25b8ad5287
bacon1097 <brunyeeb@gmail.com>
2022-04-29 09:04:19 +0100

    ensure email is lower case

--- functions/src/triggers/onCreateUser.ts
+++ functions/src/triggers/onCreateUser.ts
@@ -6,9 +6,12 @@ export default async (
   snapshot: QueryDocumentSnapshot,
   context: EventContext
 ) => {
+  const email = snapshot.data()?.email;
+
   try {
     firestore.doc(snapshot.ref.path).update({
       createdAt: snapshot.createTime,
+      ...(email && { email: String(email).toLowerCase() }),
     });
   } catch (e) {
     logger.error("Failed to add createdAt field to user: ", e);
